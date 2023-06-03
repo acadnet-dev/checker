164c3d285f6db3817d7e59390251f2a4a7d6340d
Dimitrie David <david.dimitrie@gmail.com>
2023-06-03 20:59:15 +0300

    test pod creation

--- checker.py
+++ checker.py
@@ -4,12 +4,19 @@ from api import run
 from kubernetes import client, config
 
 if __name__ == '__main__':
+    # run()
     config.load_incluster_config()
 
     v1 = client.CoreV1Api()
-    print("Listing pods with their IPs:")
-    ret = v1.list_pod_for_all_namespaces(watch=False)
-    for i in ret.items:
-        print("%s\t%s\t%s" %
-              (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
-    # run()
+
+    with open(path.join(path.dirname(__file__), "sandbox-cpp-deployment.yaml")) as f:
+        dep = yaml.safe_load(f)
+
+        # add random id to deployment name
+        deployment_id = str(uuid.uuid4())
+        deployment_name = dep["metadata"]["name"] + "-" + deployment_id
+        dep["metadata"]["name"] = deployment_name
+
+        resp = k8s_apps_v1.create_namespaced_pod(
+            body=dep, namespace="acadnet")
+        print(f"Pod created with name {deployment_name}")
--- sandbox_adapter.py
+++ sandbox_adapter.py
@@ -3,6 +3,7 @@ from config import Config
 import docker
 import time
 from kubernetes import client, config
+import yaml
 
 class SandboxCreator:
     def __init__(self):
@@ -29,20 +30,43 @@ class SandboxCreator:
 
             return container.id
         else:
-            pass
-            # TODO: create sandbox in k8s
+            config.load_incluster_config()
 
-    def get_sandbox_endpoint(self, container_id):
+            v1 = client.CoreV1Api()
+
+            with open(path.join(path.dirname(__file__), "sandbox-cpp-deployment.yaml")) as f:
+                dep = yaml.safe_load(f)
+
+                # add random id to deployment name
+                deployment_id = str(uuid.uuid4())
+                deployment_name = dep["metadata"]["name"] + "-" + deployment_id
+                dep["metadata"]["name"] = deployment_name
+
+                resp = k8s_apps_v1.create_namespaced_pod(
+                    body=dep, namespace="acadnet")
+                print(f"Pod created with name {deployment_name}")
+            
+            # wait for sandbox to start maxim 10 seconds
+            for i in range(10):
+                resp = v1.list_namespaced_pod(
+                    namespace="default", label_selector=f"app=sandbox-cpp,deployment={deployment_id}")
+                if len(resp.items) > 0:
+                    break
+                time.sleep(1)
+
+            return deployment_name
+
+    def get_sandbox_endpoint(self, id):
         if self.config.is_development():
-            container = self.client.containers.get(container_id)
+            container = self.client.containers.get(id)
             print(f"Launched docker sandbox on port {container.ports['2999/tcp'][0]['HostPort']} with id {container.id}")
             return f"http://localhost:{container.ports['2999/tcp'][0]['HostPort']}"
         else:
             pass
 
-    def stop_sandbox(self, container_id):
+    def stop_sandbox(self, id):
         if self.config.is_development():
-            container = self.client.containers.get(container_id)
+            container = self.client.containers.get(id)
             container.stop()
             print(f"Stopped sandbox with id {container.id}")
         else:
