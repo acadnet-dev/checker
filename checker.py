84a86cfc72bc2cd85b254a4c0e8a005f25c474e2
Dimitrie David <david.dimitrie@gmail.com>
2023-06-03 21:30:25 +0300

    dev

--- checker.py
+++ checker.py
@@ -8,34 +8,40 @@ import uuid
 import time
 
 if __name__ == '__main__':
-    # run()
-    config.load_incluster_config()
+    try:
+        # run()
+        config.load_incluster_config()
 
-    v1 = client.CoreV1Api()
+        v1 = client.CoreV1Api()
 
-    with open(path.join(path.dirname(__file__), "sandbox-cpp-deployment.yaml")) as f:
-        pod = yaml.safe_load(f)
+        with open(path.join(path.dirname(__file__), "sandbox-cpp-deployment.yaml")) as f:
+            pod = yaml.safe_load(f)
 
-        # add random id to deployment name
-        pod_id = str(uuid.uuid4())
-        pod_name = pod["metadata"]["name"] + "-" + pod_id
-        pod["metadata"]["name"] = pod_name
+            # add random id to deployment name
+            pod_id = str(uuid.uuid4())
+            pod_name = pod["metadata"]["name"] + "-" + pod_id
+            pod["metadata"]["name"] = pod_name
 
-        resp = v1.create_namespaced_pod(
-            body=pod, namespace="acadnet")
-        print(f"Pod created with name {pod_name}")
+            resp = v1.create_namespaced_pod(
+                body=pod, namespace="acadnet")
+            print(f"Pod created with name {pod_name}")
 
-    # wait for sandbox to start maxim 10 seconds
-    for i in range(10):
-        pod = v1.read_namespaced_pod_log(pod_name, "acadnet")
-        if "Uvicorn running" in pod:
-            break
-        time.sleep(1)
+        # wait for sandbox to start maximum 10 seconds
+        for i in range(30):
+            status = v1.read_namespaced_pod_status(pod_name, "acadnet")
+            if status.status.phase == "Running":
+                pod = v1.read_namespaced_pod_log(pod_name, "acadnet")
+                if "Uvicorn running" in pod:
+                    break
+            time.sleep(1)
+            print("Waiting for sandbox to start")
 
-    if "Uvicorn running" not in pod:
-        raise Exception("Sandbox failed to start")
-    
-    print("Sandbox online")
+        if "Uvicorn running" not in pod:
+            raise Exception("Sandbox failed to start")
+        
+        print("Sandbox online")
+    except Exception as e:
+        print(e)
 
     while True:
         time.sleep(1)
